e1a520bf1aaa5f7f0467cd6104730e18e135adc1
MrugankaChiplunkar <146470518+MrugankaChiplunkar@users.noreply.github.com>
2025-04-02 19:13:25 +0530

    Update DBConnUtil.java

---
+++ case-study-project-management-system/src/util/DBConnUtil.java
@@ -0,0 +1,21 @@
+package util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnUtil {
+	    private static Connection conn;
+
+	    static {
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");  
+	            conn = DriverManager.getConnection("jdbc:mysql://localhost:1433/projectmanagementsystem", "root", "********");
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    public static Connection getConnection() {
+	        return conn;
+	    }
+	}
