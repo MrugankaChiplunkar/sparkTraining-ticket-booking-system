768e885c34f5ed8a7b9832b9177557397cd760bc
MrugankaChiplunkar <146470518+MrugankaChiplunkar@users.noreply.github.com>
2025-04-02 09:36:21 +0530

    Update ProjectRepositoryImpl.java

--- case-study-project-management-system/src/dao/ProjectRepositoryImpl.java
+++ case-study-project-management-system/src/dao/ProjectRepositoryImpl.java
@@ -1,5 +1,17 @@
 package dao;
+import entity.employee;
+import entity.project;
+import entity.task;
+import java.util.List;
 
-public class ProjectRepositoryImpl {
+public interface IProjectRepository {
 
+	boolean createEmployee(employee emp);
+	boolean createProject(project pj);
+	boolean createTask(task tk);
+	boolean assignProjectToEmployee(int project_id, int employee_id);
+	boolean assignTaskInProjectToEmployee(int task_id, int project_id, int employee_id);
+	boolean deleteEmployee(int employee_id);
+	boolean deleteProject(int project_id);
+	List<task> getAllTasks(int employee_id, int project_id);
 }
