4b9d31e9db7f159977e6cddfeb15d56b78cd5448
MrugankaChiplunkar <146470518+MrugankaChiplunkar@users.noreply.github.com>
2025-04-06 22:08:16 +0530

    Update IBookingSystemServiceProvider.java

--- ticket-booking-system/src/dao/IBookingSystemServiceProvider.java
+++ ticket-booking-system/src/dao/IBookingSystemServiceProvider.java
@@ -1,5 +1,25 @@
 package dao;
+import entity.Booking;
+import entity.Concert;
+import entity.Customer;
+import entity.Event;
+import entity.Movie;
+import entity.Sports;
+import entity.Venue;
 
-public class IBookingSystemServiceProvider {
+import java.sql.SQLException;
+import java.time.*;
+import java.util.List;
 
+import exception.EventNotFoundException;
+import exception.InvalidBookingIDException;
+import exception.NullPointerException;
+
+public interface IBookingSystemServiceProvider {
+	public List<Event> getAvailableNoOfTickets(String event_name);
+	public boolean calculateBookingCost(int num_tickets, String event_name) throws SQLException;
+	public boolean bookTickets(String event_name, int num_tickets, LocalDate booking_date, String customer_name, 
+			String email, String phone_number);
+	public boolean cancelBooking(int booking_id);
+	public boolean getBookingDetails(int booking_id);
 }
