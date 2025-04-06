154d7313c85b8e9ae53e7f6d2f5f6447812148c5
MrugankaChiplunkar <146470518+MrugankaChiplunkar@users.noreply.github.com>
2025-04-06 22:08:31 +0530

    Update IEventServiceProvider.java

--- ticket-booking-system/src/dao/IEventServiceProvider.java
+++ ticket-booking-system/src/dao/IEventServiceProvider.java
@@ -1,5 +1,18 @@
 package dao;
+import entity.Booking;
+import entity.Concert;
+import entity.Customer;
+import entity.Event;
+import entity.Movie;
+import entity.Sports;
+import entity.Venue;
+import java.time.*;
+import java.util.List;
+import exception.EventNotFoundException;
+import exception.InvalidBookingIDException;
+import exception.NullPointerException;
 
-public class IEventServiceProvider {
-
+public interface IEventServiceProvider {
+	public boolean createEvent(Event event);
+	public List<Event> getEventDetails(String event_name);
 }
